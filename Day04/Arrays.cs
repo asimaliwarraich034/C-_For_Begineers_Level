62895a3c3a207503b9fe079a481577ec88f82fcc
asimaliwarraich034 <asimalij2923@gmail.com>
2024-03-11 06:53:57 +0500

    Arrays in c# concepts are mentioned , and write the details inMyLearning_Notes files also

---
+++ Day04/Arrays.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace Introductio_To_CSharp.Day04
+{
+     class Arrays
+    {
+        static void Main()
+        {
+            /*int[] EvenNumbers = { 2, 4, 6, 8, 10, 12, 14 };
+            Console.WriteLine("Elemnet at Index 2 is: {0} " ,EvenNumbers[2]);*/
+
+            // Array cannot grow in size once  initialized
+            string[] Friend_List = new string [3];
+            Friend_List[0] = "Ahmed Raza";
+            Friend_List[1] = "Asif Ali";
+            Friend_List[2] = "Muhhamd Sheraz";
+            Console.WriteLine(Friend_List[1]);
+        }
+        
+    }
+}
