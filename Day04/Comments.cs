e85728c87e320bbdc7f0fb4aa5661be780f3f616
asimaliwarraich034 <asimalij2923@gmail.com>
2024-03-11 07:08:44 +0500

    Comments in c#

---
+++ Day04/Comments.cs
@@ -0,0 +1,41 @@
+﻿
+
+using System.Reflection.Metadata;
+using System.Runtime.InteropServices;
+
+namespace Introductio_To_CSharp.Day04
+{
+   class Comments
+    {
+
+        /*int[] EvenNumbers = { 2, 4, 6, 8, 10, 12, 14 };
+           Console.WriteLine("Elemnet at Index 2 is: {0} " ,EvenNumbers[2]);*/
+
+        // ...Single line comment 
+
+        // Array cannot grow in size once  initialized 
+
+        //.. Below is UserStringHandle foe multi line code ..
+
+        /* string[] Friend_List = new string[3];
+         Friend_List[0] = "Ahmed Raza";
+             Friend_List[1] = "Asif Ali";
+             Friend_List[2] = "Muhhamd Sheraz";
+             Console.WriteLine(Friend_List[1]); */
+
+        static void Main()
+        {
+            SampleClass
+        }
+
+
+        /// <summary>
+        /// This is a sample class and sample documentation 
+        /// </summary>
+        public class SampleClass
+        {
+            
+        }
+    }
+}
+
