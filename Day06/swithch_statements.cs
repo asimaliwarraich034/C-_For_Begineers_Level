bae858556301765140e14ec67899a971636bd583
asimaliwarraich034 <asimalij2923@gmail.com>
2024-03-19 22:16:32 +0500

    Switch statements in C#  Basic understanding

---
+++ Day06/swithch_statements.cs
@@ -0,0 +1,44 @@
+﻿
+
+namespace Introductio_To_CSharp.Day06
+{
+  class swithch_statements
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please Entered the Number:");
+            
+            int UserNumber = int.Parse(Console.ReadLine());
+
+            /*switch (UserNumber) {
+                case 10:
+                    Console.WriteLine("Your enterdthe number 10");
+                    break;
+                    case 20:
+                    Console.WriteLine("Ypu Entered the Number 20");
+                    break;  
+                    case 30:
+                    Console.WriteLine("Yopu Entered the Number 30");
+                    break;
+
+                    default:
+                    Console.WriteLine("You not entered the 10, 20 & 30");
+                    break;*/
+
+            switch (UserNumber)
+            {
+                case 10:
+                    
+                case 20:
+                   
+                case 30:
+                    Console.WriteLine("You Entered the Number {0}" , UserNumber);
+                    break;
+
+                default:
+                    Console.WriteLine("You not entered the 10, 20 & 30");
+                    break;
+            }
+        }
+    }
+}
