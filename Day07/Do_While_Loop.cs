1f699065a81727774c00cdc8b7f3d8d0e8824262
asimaliwarraich034 <asimalij2923@gmail.com>
2024-03-20 15:16:12 +0500

    Do while loop understanding,  and noted the difference b/w The while loop and the do while loop

---
+++ Day07/Do_While_Loop.cs
@@ -0,0 +1,35 @@
+﻿
+
+namespace Introductio_To_CSharp.Day07
+{
+    class Do_While_Loop
+    {
+        static void Main()
+        {
+            string UserChoice = String.Empty;
+
+            do
+            {
+                Console.WriteLine("Please Entered your Target Number");
+                int UserTarget = int.Parse(Console.ReadLine());
+                int start = 0;
+                while (start <= UserTarget)
+                {
+                    Console.Write(start + " ");
+                    start = start + 2;
+                }
+
+                do
+                {
+                    Console.WriteLine(" Do you want to continue -- Yes or No ?");
+                    UserChoice = Console.ReadLine().ToUpper();
+                    if (UserChoice != "YES" && UserChoice != "NO")
+                    {
+                        Console.WriteLine("Invalid Choice Please select Yes or No ");
+                    }
+                } while (UserChoice != "YES" && UserChoice != "NO");
+            } while (UserChoice == "YES");
+            }
+
+    }
+}
