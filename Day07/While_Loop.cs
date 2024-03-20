08aff96c72775757a23e46efcf6c45150919a659
asimaliwarraich034 <asimalij2923@gmail.com>
2024-03-20 14:14:48 +0500

    while loop Basics  Understanding and keep notes

---
+++ Day07/While_Loop.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace Introductio_To_CSharp.Day07
+{
+  class While_Loop
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please Entered the  Traget Number");
+             int UserTraget = int.Parse(Console.ReadLine());
+
+            int start =0;
+
+            while (start <= UserTraget )
+            {
+                Console.Write(start + " ");
+                start = start + 2;
+            }
+        }
+    }
+}
