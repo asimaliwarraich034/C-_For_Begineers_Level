028f7e1d995d55c0e555f8fd1943ebc0d07d2483
asimaliwarraich034 <asimalij2923@gmail.com>
2024-03-25 06:42:17 +0500

    Methods in C# , static and Instance methods Understanding andlogic building

---
+++ Day09/Methods_InCsharpp.cs
@@ -0,0 +1,51 @@
+﻿
+
+namespace Introductio_To_CSharp.Day09
+{
+     class Methods_InCsharpp
+    {
+        /*static void Main()
+        {
+            Methods_InCsharpp m = new Methods_InCsharpp();
+            m.EvenNumber();
+        }
+
+        public void EvenNumber()
+        {
+            int start = 0;
+            while (start <= 10)
+            {
+                Console.WriteLine(start);
+                start = start + 2;
+            }
+        }*/
+
+        /// <summary>
+        ///  make the static methpds and use it 
+        /// </summary>
+        static void Main()
+        {
+            Methods_InCsharpp.EvenNumber(30);
+            Methods_InCsharpp s = new Methods_InCsharpp();
+            int sum = s.Add(10, 20);
+            Console.WriteLine("The sum is = {0}" , sum);
+        }
+
+        public int Add(int fn , int sn)
+        {
+            return fn + sn;
+        }
+
+        public static void EvenNumber(int Target)
+        {
+            int start = 0;
+
+            while (start <= Target)
+            {
+                Console.WriteLine(start);
+                start = start + 2;
+            }
+        }
+
+    }
+}
