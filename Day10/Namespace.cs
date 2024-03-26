f3b9d732319ca21b059651445dea507287f49691
asimaliwarraich034 <asimalij2923@gmail.com>
2024-03-26 19:55:33 +0500

    Namespaces and how they can use to organize code and avoid name clashes

---
+++ Day10/Namespace.cs
@@ -0,0 +1,49 @@
+﻿//using.ProectA.TeamA
+//using ProectA.TeamB  // Using Directives
+
+using PATA = ProjectA.TeamA;
+using PATB = ProjectA.TeamB;
+
+namespace Introductio_To_CSharp.Day10
+{
+     class Namespace
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello");
+
+            //ProjectA.TeamA.ClassA.Print(); // Fully Qualified 
+           PATA.ClassA.Print();
+            PATB.ClassA.Print();
+        }
+        
+
+    }
+}
+
+namespace ProjectA
+{
+    namespace TeamA
+    {
+        class ClassA
+        {
+            public static void Print()
+            {
+                Console.WriteLine("Team A print Method");
+            }
+        }
+    }
+}
+namespace ProjectA
+{
+    namespace TeamB
+    {
+        class ClassA
+        {
+            public static void Print()
+            {
+                Console.WriteLine("Team B print Method");
+            }
+        }
+    }
+}
