667b07fe9c2df8fb1259b4017f1354eadd258358
asimaliwarraich034 <asimalij2923@gmail.com>
2024-03-26 22:47:35 +0500

    Static and Instance class members
    Difference between static and instance members
    An example explaining when you should make certain members static

---
+++ Day10/Static_Instance_ClassMembers.cs
@@ -0,0 +1,42 @@
+﻿
+
+namespace Introductio_To_CSharp.Day10
+{ 
+    class Circle
+{
+   static float _P1;
+    int _Radius;
+        static Circle()
+        {
+            Console.WriteLine("Static Constructor called");
+            Circle._P1 = 3.14F;
+        }
+
+    public Circle(int radius)
+           
+    {
+            Console.WriteLine("Instance Constructor called");
+
+            this._Radius = radius;   
+    }
+
+    public float   CalcArea()
+    {
+       return _P1 * this._Radius * this._Radius;
+    }
+}
+
+
+    class Static_Instance_ClassMembers
+    {
+        static void Main()
+        {
+        Circle c1 = new Circle(4);
+       float Area =  c1.CalcArea();
+        Console.WriteLine("Area of A Circle = {0} " , Area);
+            Circle c2 = new Circle(5);
+            float Area2 = c2.CalcArea();
+            Console.WriteLine("Area of A Circle = {0} ", Area2);
+        }
+    }
+}
