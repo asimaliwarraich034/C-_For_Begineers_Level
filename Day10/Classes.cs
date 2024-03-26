79f9f2bbb6ea08dda408dc3ba5ea5a940405dc37
asimaliwarraich034 <asimalij2923@gmail.com>
2024-03-26 21:39:22 +0500

    class , constructor , Overloading class constructor , this keyword ,Destructors

---
+++ Day10/Classes.cs
@@ -0,0 +1,48 @@
+﻿
+
+namespace Introductio_To_CSharp.Day10
+{
+
+
+    class Customer
+    {
+        string _firstName;
+        string _lastName;
+
+        public Customer()
+        :this("No FirstName Provide" , "No LastName provise")
+        {
+        }
+        public Customer(string FirstName, string LastName)
+        {
+            this._firstName = FirstName;
+            this._lastName = LastName;
+        }
+        ~Customer()
+        {
+            //Clean up the code
+        }
+
+        public void PrintFullName()
+        {
+            Console.WriteLine("Full Name is = {0}", this._firstName + " " + this._lastName);
+        }
+    }
+
+
+
+    class Classes
+    {
+        static void Main()
+        {
+            //Customer c2 = new Customer("Asim", "Ali");
+            Customer c2 = new Customer(); // Parameters less customer 
+            c2.PrintFullName();
+            // OverLoad Constructor ( With the number and type Parameters) 
+            Customer c3 = new Customer("Hashim", "Ali");
+            c3.PrintFullName(); 
+        }
+
+    }
+
+}
