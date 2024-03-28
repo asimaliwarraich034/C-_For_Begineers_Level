f15d473e73c24f96747397885c542a03b311259e
asimaliwarraich034 <asimalij2923@gmail.com>
2024-03-28 18:19:22 +0500

    Polymorphism , Overriding virtual methods

---
+++ Day12/Polymorphism.cs
@@ -0,0 +1,60 @@
+﻿
+
+namespace Introductio_To_CSharp.Day12
+{
+    public class NewInternees
+    {
+        public string FirstName = "FN";
+        public string LastName = "LN";
+
+
+        public virtual void ShowInternneDetails()
+        {
+            Console.WriteLine("Name: {0} {1} ", FirstName, LastName);
+            
+        }
+    }
+
+    public class FullTimeInternee :NewInternees
+    {
+        public override void ShowInternneDetails()
+        {
+            Console.WriteLine( FirstName + " " + LastName + " " + " - Full Time Internee");
+
+        }
+
+    }
+    public class PartTimeInternee : NewInternees
+    {
+        public override void ShowInternneDetails()
+        {
+            Console.WriteLine(FirstName + " " + LastName + " " + " - PartTimeInternee");
+        }
+    }
+    public class TemporarayInternee : NewInternees
+    {
+       // Base class virtual method is used 
+
+    }
+
+    internal class Polymorphism
+    {
+        static void Main()
+        {
+            NewInternees[] internees = new NewInternees[4];
+            internees[0] = new NewInternees();
+            internees[1] = new FullTimeInternee(); // base class reference variable can point to child class Object
+            internees[2] = new PartTimeInternee();
+            internees[3] = new TemporarayInternee();
+
+            foreach (NewInternees item in internees)
+            {
+                item.ShowInternneDetails();
+            }
+
+
+
+        }
+
+    }
+}
