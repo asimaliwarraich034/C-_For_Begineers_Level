c07f83ef51871d813b42bb406e5a744f4fd27672
asimaliwarraich034 <asimalij2923@gmail.com>
2024-03-30 15:25:02 +0500

    Method Ovveriding vs Method Hiding

---
+++ Day12/MethodOverrideing_MethodHiding.cs
@@ -0,0 +1,33 @@
+﻿
+
+namespace Introductio_To_CSharp.Day12
+{
+    public class BaseClass
+    {
+        public virtual void Print()
+        {
+            Console.WriteLine("I am a Base Class Method");
+        }
+    }
+
+    public class DerivedClass: BaseClass
+    {
+        // ....... Method Ovverriding  ..........
+       // public override void Print() 
+
+        //........... Method Hiding ---------
+        public new void Print()
+
+        {
+            Console.WriteLine("I am a Derived Class Method");
+        }
+    }
+    class MethodOverrideing_MethodHiding
+    {
+       public static void Main()
+        {
+            BaseClass BF = new DerivedClass();
+            BF.Print();
+        }
+    }
+}
