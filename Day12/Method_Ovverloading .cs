7c922ab487fbf389a5265c40f4c52ca1ec5fb246
asimaliwarraich034 <asimalij2923@gmail.com>
2024-03-30 15:58:50 +0500

    Method Overloadimng and its key concepts

---
+++ Day12/Method_Ovverloading .cs
@@ -0,0 +1,54 @@
+﻿
+
+/*namespace Introductio_To_CSharp.Day12
+{
+     class Method_Ovverloading
+    {
+        public static void Main()
+        {
+            
+        }
+
+        // Ovverloaded on basis of Kind of Parameters
+        // Ovverloaded on basis of Number of Parameters
+        //Ovverloaded on basis of Types of Parameters
+        public static void Add(int FN , int SN ,out int sum)
+        {
+            sum = FN + SN ;
+        }
+
+        public static void Add(int FN, int SN, int Ln)
+        {
+            Console.WriteLine("Sum = {0}", FN + SN);
+        }
+
+        public static void Add(int FN, int SN, out int sum)
+        {
+            sum = FN + SN;
+        }
+
+        public static void Add(int FN, int SN, float Ln)
+        {
+            Console.WriteLine("Sum = {0}", FN + SN + Ln );
+        }
+
+        // Not possible to ovverload a method based on Paramas keywords
+        public static void Add(int FN, int SN, params int [] Ln)
+        {
+            Console.WriteLine("Sum = {0}", FN + SN );
+        }
+        public static void Add(int FN, int SN, int[] Ln)
+        {
+            Console.WriteLine("Sum = {0}", FN + SN);
+        }
+        //Not possible to ovverload a method based on Return Type
+        public static void Add(int FN, int SN, float Ln)
+        {
+            Console.WriteLine("Sum = {0}", FN + SN + Ln);
+        }
+        public static int  Add(int FN, int SN, float Ln)
+        {
+            Console.WriteLine("Sum = {0}", FN + SN + Ln);
+        }
+    }
+} */
