92de274a617daff94e8d8150c853bc589b9c53c5
asimaliwarraich034 <asimalij2923@gmail.com>
2024-04-02 23:29:00 +0500

    Abstract Classes Vs Interfaces

---
+++ Day13/AbstractClass_Vs_ Interfaces.cs
@@ -0,0 +1,31 @@
+﻿
+
+namespace Introductio_To_CSharp.Day13
+{
+    public abstract class new_Person
+    {
+        int ID;
+        public void callMethod()
+        {
+
+        }
+    }
+
+    // Interface not have implementation only declaration
+    interface MyInterface
+    {
+        // No fields in Interface
+        int Name;
+        void print();
+        
+    } 
+    
+        
+    public class AbstractClass_Vs__Interfaces
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
