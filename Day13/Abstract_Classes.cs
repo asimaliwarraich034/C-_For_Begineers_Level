618974b035d98df6970b1bc81cb1553ca01c3c90
asimaliwarraich034 <asimalij2923@gmail.com>
2024-04-02 23:05:52 +0500

    Abstract class

---
+++ Day13/Abstract_Classes.cs
@@ -0,0 +1,39 @@
+﻿
+
+namespace Introductio_To_CSharp.Day13
+{
+    public abstract class Person_Details
+    {
+        //public abstract void print();
+
+        //Abstract class may contain abstracty members
+        //(methods, properites,indexes and event, etc)but not mendatory
+          
+         public void print()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+    public   class   Abstract_Classes  : Person_Details    {
+      // public  void print()
+        //{
+            
+        //}
+        public static void Main()
+        {
+            // Parent class refence variable can point
+            // to the derived class object 
+
+            //Person_Details P1 = new Abstract_Classes(); 
+            //P1.print(); 
+
+            // Craete instance of class 
+
+            Abstract_Classes P2 = new Abstract_Classes();
+            P2.print();
+
+
+        }
+    }
+}
+
