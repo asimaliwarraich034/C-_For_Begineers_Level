c42b60e8b470695371d1e998375e574d25aac56e
asimaliwarraich034 <asimalij2923@gmail.com>
2024-04-02 22:31:09 +0500

    explicit interface implementation and Default interface implementation

---
+++ Day13/Explicit_Interface_Implimentation.cs
@@ -0,0 +1,40 @@
+﻿
+
+namespace Introductio_To_CSharp.Day13
+{
+    interface IMyIntro
+    {
+        void print();
+    }
+    interface IMyIntro2
+    {
+        void print();
+    }
+    public class Explicit_Interface_Implimentation : IMyIntro, IMyIntro2
+    {
+        // Explicit Interface Method
+       void IMyIntro.print()
+        {
+            Console.WriteLine("My name is Asim Ali");
+        }
+
+        //  IMyIntro2 Default interface method
+        public void print()
+        {
+            Console.WriteLine("My name is Hashim Ali");
+        }
+        public static void Main()
+        {
+            Explicit_Interface_Implimentation M1 = new Explicit_Interface_Implimentation(); 
+            //((IMyIntro)M1).print(); Typecaset
+            //((IMyIntro2)M1).print();    
+
+            IMyIntro I1 = new Explicit_Interface_Implimentation();
+            IMyIntro2 I2 = new Explicit_Interface_Implimentation();
+            
+
+            I1.print();
+            I2.print();
+        }
+    }
+}
