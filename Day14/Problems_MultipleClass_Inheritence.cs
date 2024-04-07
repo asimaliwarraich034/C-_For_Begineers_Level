79aff541886537496896c2f7b012a71a1cf5434d
asimaliwarraich034 <asimalij2923@gmail.com>
2024-04-07 17:22:01 +0500

    Problem with multiple class Inheritence

---
+++ Day14/Problems_MultipleClass_Inheritence.cs
@@ -0,0 +1,44 @@
+﻿
+
+namespace Introductio_To_CSharp.Day14
+{
+   public class A
+    {
+        public virtual void Print()
+        {
+            Console.WriteLine("A class Implementation");
+        }
+    }
+
+    public class    B : A
+    {
+        public override void Print()
+        {
+            Console.WriteLine("B class Implementation");
+        }
+    }
+
+    public class C : A
+     {
+        public override void Print()
+        {
+            Console.WriteLine(" C class Implementation");
+        }
+    }
+
+    // C# No support multile class Inheritence 
+    /*public class D :B, C
+    {
+        
+    }*/
+    public  class Problems_MultipleClass_Inheritence
+    {
+        public static void Main()
+        {
+            //D d1 = new D();
+            //d1.Print();
+
+
+        }
+    }
+}
