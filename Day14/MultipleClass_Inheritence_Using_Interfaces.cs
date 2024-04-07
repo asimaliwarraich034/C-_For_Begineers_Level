dad594f6d2c6639abc447e4b87f6fa1d873f0957
asimaliwarraich034 <asimalij2923@gmail.com>
2024-04-07 17:21:11 +0500

    Multiplee class Inheritence

---
+++ Day14/MultipleClass_Inheritence_Using_Interfaces.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Introductio_To_CSharp.Day14
+{
+    public class MultipleClass_Inheritence_Using_Interfaces
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
