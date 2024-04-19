52ab75ab07adc8ba44ef82e5432448fcb1658551
asimaliwarraich034 <asimalij2923@gmail.com>
2024-04-19 17:29:26 +0500

    Exception handling using try, catch and finally blocks

---
+++ Day16/ExceptionHandling.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+
+namespace Introductio_To_CSharp.Day16
+{
+    public class ExceptionHandling
+    {
+        public static void Main()
+        {
+            StreamReader streamReader = null;
+            try
+            {
+                streamReader  = new StreamReader(@"U:\Data Folder2\data.txt");
+                Console.WriteLine(streamReader.ReadToEnd());
+                
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("Please check if the file {0} exsist", ex.FileName);
+            }
+            catch (Exception ex) {
+                Console.Write(ex.Message);
+            }
+            finally
+            {
+                if (streamReader != null)
+                {
+                    streamReader.Close();
+                }
+                Console.WriteLine("Finally Block");
+            }
+            
+        }
+    }
+}
