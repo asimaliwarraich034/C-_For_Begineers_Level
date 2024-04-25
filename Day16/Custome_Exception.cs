048a63e694f7656dd004df3607acf8559a2dbf30
asimaliwarraich034 <asimalij2923@gmail.com>
2024-04-25 22:44:25 +0500

    Custome EXCEPTION from scratch

---
+++ Day16/Custome_Exception.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+
+namespace Introductio_To_CSharp.Day16
+{
+    
+    public  class Custome_Exception
+    {
+        public static void Main()
+        {
+            try { 
+
+
+            throw new UseralreadyLoggedInException("User already logged-in -> No dupplicate session");
+        } catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+    [Serializable]
+    public class  UseralreadyLoggedInException: Exception 
+    
+    {
+        public UseralreadyLoggedInException() :base()  // overload constructor 
+        { 
+        
+        }
+        public UseralreadyLoggedInException(string message) : base(message) 
+        {
+        
+        }
+
+        public UseralreadyLoggedInException(string message, Exception innerException)
+          : base(message, innerException)
+        {
+        }
+       public UseralreadyLoggedInException(SerializationInfo info, StreamingContext context)    
+            :base(info,context)
+        {
+
+        }
+
+    }
+}
