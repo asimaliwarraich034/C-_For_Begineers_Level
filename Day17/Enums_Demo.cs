112ff4eb8d205cd7c015635c8f7039b7b2de86f4
asimaliwarraich034 <asimalij2923@gmail.com>
2024-05-02 22:19:13 +0500

    purpose of enums and the problems of not using enums

---
+++ Day17/Enums_Demo.cs
@@ -0,0 +1,54 @@
+﻿
+
+using System.Diagnostics.Contracts;
+
+namespace Introductio_To_CSharp.Day17
+{
+    public class Enums_Demo
+    {
+        public static void Main()
+        {
+            Customer[] customers = new Customer[3];
+            customers[0] = new Customer
+            {
+                Name = "Asim",
+                Gender = 1
+            };
+            customers[1] = new Customer
+            {
+                Name = "aleena",
+                Gender = 0
+            };
+            customers[2] = new Customer
+            {
+                Name = "katreena",
+                Gender = 2
+            };
+            foreach (Customer customer in customers)
+            {
+                Console.WriteLine("Name ={0} && Gender ={1}", customer.Name, GetGender(customer.Gender));
+            }
+            
+        }
+         public static string GetGender (int gender)
+        {
+            switch(gender)
+            {
+                case 0:
+                    return "unknown";
+                case 1:
+                    return "Male";
+                    case 2:
+                    return "Female";
+                default:
+                    return "unknown data detected";
+                  
+            }
+        }
+    }
+}
+public class Customer
+{
+  public string Name { get; set; }
+    public int Gender { get; set; }
+}
