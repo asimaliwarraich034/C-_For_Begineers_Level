0617578f41ac8d4978417d2e1a7ffc47aca36c1b
asimaliwarraich034 <asimalij2923@gmail.com>
2024-05-10 21:50:09 +0500

    Purpose of attribute , Using an attribute in sample program And Customizing attribute  using parameters

---
+++ Day19/Attributes.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Introductio_To_CSharp.Day19
+{
+    public class Attributes
+    {
+        public static void Main()
+        {
+            //Console.WriteLine(Calculator.Add(12, 12));
+            Console.WriteLine(Calculator.Add(new List<int>() { 12, 12,10,44 }));
+        }
+    }
+}
+
+public class Calculator
+{
+    [Obsolete ("Use Add(List<int> Numbers) Methods")]
+    public static int Add(int FirstNum, int SecondNum)
+    {
+        return FirstNum + SecondNum;
+    }
+
+    public static int Add(List<int> Numbers)
+    {
+        int Sum = 0;
+        foreach (int Number in Numbers)
+        {
+            Sum = Sum + Number;
+        }
+        return Sum;
+    }
+
+}
