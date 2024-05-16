28e37fd8ef16f4d7e4a168a38441cc86acd1d552
asimaliwarraich034 <asimalij2923@gmail.com>
2024-05-16 15:39:19 +0500

    Why should you override ToString Method

---
+++ Day20/override_ToString_Method.cs
@@ -0,0 +1,34 @@
+﻿
+
+namespace Introductio_To_CSharp.Day20
+{
+    public class override_ToString_Method
+    {
+        public static void Main()
+
+        {
+            int Number = 11;
+            Console.WriteLine(Number.ToString());
+            person_info p1 = new person_info();
+            p1.firstName = "Kamran";
+            p1.lastName = "Akmal";
+
+            Console.WriteLine(p1.ToString());
+            Console.WriteLine(Convert.ToString(p1));
+
+        }
+    }
+    public class person_info
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        public override string ToString()
+        {
+            //return this.firstName + " " + this.lastName;
+            return this.lastName + " " + this.firstName; 
+        }
+
+    }
+
+}
