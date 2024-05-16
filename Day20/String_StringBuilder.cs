b0639969afeeeacb98154fe32b833150e72b75cc
asimaliwarraich034 <asimalij2923@gmail.com>
2024-05-16 17:12:36 +0500

    difference between System.String and System.Text.or StringBuilder classes

---
+++ Day20/String_StringBuilder.cs
@@ -0,0 +1,30 @@
+﻿
+
+using System.Text;
+
+namespace Introductio_To_CSharp.Day20
+{
+    public class String_StringBuilder
+    {
+        public static void Main() {
+
+            //string Course_Content = "C# ";
+            //Course_Content += "Video ";
+            //Course_Content += "Tutorial ";
+            //Course_Content += "For ";
+            //Course_Content += "Begineers ";
+
+            //Using String Builder
+
+            StringBuilder  Course_Content = new StringBuilder ("C# ");
+            Course_Content.Append("Video ");
+            Course_Content.Append ("Tutorial ");
+            Course_Content.Append  ("For ");
+            Course_Content.Append  ("Begineers ");
+
+            // Console.WriteLine(Course_Content); // For string 
+
+            Console.WriteLine (Course_Content.ToString ()); // For String Builder 
+            }
+    }
+}
