cd702e8f71bbba6c83a50147e1e09139601252d8
asimaliwarraich034 <asimalij2923@gmail.com>
2024-05-17 16:16:53 +0500

    Partial Classes Concepts by the use of Asp.net forms

---
+++ Day21/PartialCustomerOne.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PartialClassesConcept
+{
+    public partial class PartialCustomer
+    {
+        private string _firstName;
+        private string _lastName;
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+    }
+}
