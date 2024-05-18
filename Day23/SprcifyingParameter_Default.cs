d90f9a40a887d83348f35909cd345c294925f4f2
asimaliwarraich034 <asimalij2923@gmail.com>
2024-05-18 13:47:58 +0500

    Secifying Default arameter

---
+++ Day23/SprcifyingParameter_Default.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introductio_To_CSharp.Day23
+{
+    public  class SpecifyingParameter_Default
+    {
+        public static void Main()
+        {
+            AddNumbers(50, 10); // optional parameter
+            AddNumbers(10,10, new int[] { 1, 2, 3 });   
+        }
+        public static void AddNumbers(int firstNumber, int secondNumber, int [] ? restofNumber = null)
+        {
+            int result = firstNumber + secondNumber;
+
+            if (restofNumber != null)
+            {
+                foreach (int i in restofNumber)
+                {
+                    result += i;
+                }
+
+            }
+            Console.WriteLine("Result = {0}", result);
+
+        }
+    }
+}
