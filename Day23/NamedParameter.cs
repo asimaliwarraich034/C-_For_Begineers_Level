88a55da8f6520df4886eb2dae4eab103fe0757f4
asimaliwarraich034 <asimalij2923@gmail.com>
2024-05-18 14:16:56 +0500

    Named Parameter

---
+++ Day23/NamedParameter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introductio_To_CSharp.Day23
+{
+    public class NamedParameter
+    { 
+        public static void Main()
+        {
+            ShowMethod(1,b:12,c:10);
+        }
+        public static void ShowMethod(int a, int b=3, int c= 2)
+        {
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("b = " + b);
+            Console.WriteLine("c = " +c );
+        }
+    }
+}
