f42c57681f9641482d5588c03381baa012637998
asimaliwarraich034 <asimalij2923@gmail.com>
2024-05-18 14:17:40 +0500

    4) Optional Attribute

---
+++ Day23/Optional_Attributes.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Introductio_To_CSharp.Day23
+{
+    public  class Optional_Attributes
+    {
+         public static void Main()
+        {
+            AddNumbers(12, 90); // this is optional parameter
+        }
+
+        public static void AddNumbers(int firstNumber, int secondNumber, [Optional] int [] restofNumber) // optonal attribue
+        {
+            int result = firstNumber + secondNumber;
+
+            if (restofNumber != null)
+            {
+                foreach (int i in restofNumber)
+                {
+                    result += i;
+                }
+
+            }
+            Console.WriteLine("Result = {0}", result);
+
+        }
+    }
+}
