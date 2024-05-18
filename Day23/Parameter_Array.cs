baf7de3e68a4a9428449bcef35574b0d758584c0
asimaliwarraich034 <asimalij2923@gmail.com>
2024-05-18 13:17:49 +0500

    Opptional Parameter . (1)Parameter Array

---
+++ Day23/Parameter_Array.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introductio_To_CSharp.Day23
+{
+    public  class Parameter_Array
+    {
+       
+        public static void Main()
+        {
+            //AddNumbers(12, 12, new object[] { 10, 10, 12 });
+            AddNumbers(12, 12);
+        }
+
+        public static void AddNumbers(int firstNumber, int secondNumber, params object[] restofNumber)
+        {
+            int result = firstNumber + secondNumber;
+
+            if (restofNumber != null)
+            {
+                foreach (int i in restofNumber)
+                {
+                    result += i;
+                }
+
+            }
+            Console.WriteLine("Result = {0}", result);
+
+        }
+    }
+}
